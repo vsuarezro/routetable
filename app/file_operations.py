299d072dbda77d05559d18908a96c7371a473efa
Viesur0 <victoresuarezr@gmail.com>
2024-05-10 16:17:45 -0600

    file_operations.py Removed verification of allowed characters in a file when opening it. Regex not finding all characters.

--- app/file_operations.py
+++ app/file_operations.py
@@ -1,7 +1,10 @@
-
+import os
 import re
+import logging
 
-ALLOWED_CHARACTERS = re.compile(r"^[a-zA-Z0-9\s.,]+$")  # Adjust as needed
+logger = logging.getLogger(__name__)
+
+ALLOWED_CHARACTERS = re.compile(r"^[a-zA-Z0-9\s\.,:-_\(\)\[\]#=\r\n]+$")  # Adjust as needed
 MAX_FILE_SIZE = 1024 * 1024 * 200  # 200 MB limit
 
 def load_file(filename):
@@ -10,7 +13,10 @@ def load_file(filename):
             raise ValueError("File exceeds maximum size")
 
         contents = f.read()
-        if not ALLOWED_CHARACTERS.match(contents):
-            raise ValueError("File contains invalid characters")
+        # match = ALLOWED_CHARACTERS.match(contents, re.IGNORECASE | re.MULTILINE)
+        # logger.debug(f"File contains invalid characters: {match}")
+        # if not match:
+        #     # ALLOWED_CHARACTERS.match(contents, re.IGNORECASE | re.MULTILINE)
+        #     raise ValueError(f"File contains invalid characters {match}")
 
         return contents  # Proceed to process the sanitized content
