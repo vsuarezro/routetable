f0b9ba6e6d0b41f5a037409d7d58458548c8ed65
Viesur0 <victoresuarezr@gmail.com>
2024-05-10 16:13:02 -0600

    Updated functions in storage.py
    still pending to define the database a single time, either orchestrator or storate and use it during the same run, defining different values is throwing errors depending on which variable is scoped. Test are invalid at this point

--- app/storage.py
+++ app/storage.py
@@ -40,94 +40,103 @@ import datetime
 import logging
 logger = logging.getLogger(__name__)  # Get a logger for the 'storage' module
 
-default_database_url = ":memory:"
+default_database_url = "routes.sqlite3"
+
+
+def get_connection(db_url: str = default_database_url):
+    """Returns a connection to the database"""
+    return sqlite3.connect(db_url)
 
 # Function to initialize the database
-def _initialize_database(db_url: str = default_database_url):
+def initialize_database(db_url: str = default_database_url):
     """Creates necessary tables if they don't exist"""
-    conn = sqlite3.connect(db_url)
-    cursor = conn.cursor()
-    cursor.execute(
-        """ 
-        CREATE TABLE IF NOT EXISTS igp_routes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Auto-incrementing ID
-            hostname TEXT NOT NULL,
-            service TEXT NOT NULL,
-            timestamp TEXT NOT NULL,            
-            route TEXT NOT NULL,
-            flags TEXT,
-            route_type TEXT,
-            route_protocol TEXT,
-            age TEXT,
-            preference TEXT,
-            next_hop TEXT,
-            interface_next_hop TEXT,
-            metric TEXT
+    logger.debug(f"Initializing database at {db_url}")
+    default_database_url = db_url
+    with get_connection(db_url) as conn:
+        conn = sqlite3.connect(db_url)
+        cursor = conn.cursor()
+        cursor.execute(
+            """ 
+            CREATE TABLE IF NOT EXISTS igp_routes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Auto-incrementing ID
+                hostname TEXT NOT NULL,
+                service TEXT NOT NULL,
+                timestamp TEXT NOT NULL,            
+                route TEXT NOT NULL,
+                flags TEXT,
+                route_type TEXT,
+                route_protocol TEXT,
+                age TEXT,
+                preference TEXT,
+                next_hop TEXT,
+                interface_next_hop TEXT,
+                metric TEXT
+            )
+        """
         )
-    """
-    )
-    conn.commit()
-    return conn
+        conn.commit()
+    return default_database_url
 
 
-# Call the initialization function when the module is imported
-conn = _initialize_database()
 
-
-def _destroy_database(database_connection: sqlite3.connect = conn):
-    if database_connection is None:
-        database_connection = sqlite3.connect(default_database_url)
-    cursor = database_connection.cursor()
-    cursor.execute("DROP TABLE IF EXISTS igp_routes")
-    database_connection.commit()
-    database_connection.close()
+def _destroy_database(url: str = default_database_url):
+    with get_connection(url) as database_connection:
+        if database_connection is None:
+            database_connection = sqlite3.connect(default_database_url)
+        cursor = database_connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS igp_routes")
+        database_connection.commit()
+        database_connection.close()
 
 
 def save_routes(
-    timestamp: str, routes: list, database_connection: sqlite3.connect = conn
+    timestamp: str, routes: list, url: str = default_database_url
 ) -> None:
     """Stores routes with a given timestamp in the SQLite database"""
     # breakpoint()
-    cursor = database_connection.cursor()
+    with get_connection(url) as database_connection:
+        cursor = database_connection.cursor()
+        logger.debug(f"Saving routes with timestamp {timestamp}")
+        logger.debug(f"first route: {routes[0]}")
+        cursor.executemany(
+            """
+            INSERT INTO igp_routes (hostname, service, timestamp, route, flags, route_type, route_protocol, age, preference, next_hop, interface_next_hop, metric) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            [
+                (
+                    route.get("hostname"),
+                    route.get("service"),
+                    timestamp,
+                    route.get("route"),
+                    route.get("flags", ""),
+                    route.get("route_type", ""),
+                    route.get("route_protocol", ""),
+                    route.get("age", ""),
+                    route.get("preference", ""),
+                    route.get("next_hop", ""),
+                    route.get("interface_next_hop", ""),
+                    route.get("metric", ""),
+                )
+                for route in routes
+            ],
+        )
+        database_connection.commit()
+    logger.debug(f"Saved routes with timestamp {timestamp}. Committing changes to database {cursor}")
 
-    cursor.executemany(
-        """
-        INSERT INTO igp_routes (hostname, service, timestamp, route, flags, route_type, route_protocol, age, preference, next_hop, interface_next_hop, metric) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-        [
-            (
-                route["hostname"],
-                route["service"],
-                timestamp,
-                route["route"],
-                route["flags"],
-                route["route_type"],
-                route["route_protocol"],
-                route["age"],
-                route["preference"],
-                route["next_hop"],
-                route["interface_next_hop"],
-                route["metric"],
-            )
-            for route in routes
-        ],
-    )
-    database_connection.commit()
-    # database_connection .close() # Close the connection after saving
 
 
-def get_routes(hostname:str, service:str, timestamp: str, database_connection: sqlite3.connect = conn) -> list:
+def get_routes(hostname:str, service:str, timestamp: str, url: str = default_database_url) -> list:
     """Retrieves routes for a hostname at a specific timestamp from the SQLite database"""
-    cursor = database_connection.cursor()
+    with get_connection(url) as database_connection:
+        cursor = database_connection.cursor()
 
-    cursor.execute("SELECT * FROM igp_routes WHERE hostname=? AND service=? AND timestamp=?", (hostname, service, timestamp,))
-    results = cursor.fetchall()
+        cursor.execute("SELECT * FROM igp_routes WHERE hostname=? AND service=? AND timestamp=?", (hostname, service, timestamp,))
+        results = cursor.fetchall()
 
     routes = [
         dict(zip([column[0] for column in cursor.description], row)) for row in results
     ]
 
-    # database_connection .close()
     return routes
 
 
@@ -137,11 +146,20 @@ def compare_routes(
     timestamp1: datetime,
     timestamp2: datetime,
     fields: list = ["route"],
-    database_connection: sqlite3.connect = conn,
+    url: str = default_database_url,
 ) -> dict:
 
-    added_deleted_routes_dict = get_added_deleted_routes(hostname, service, timestamp1, timestamp2, database_connection)
-    changed_routes_dict = changed_routes(hostname, service, timestamp1, timestamp2, database_connection)
+    added_deleted_routes_dict = get_added_deleted_routes(hostname, service, timestamp1, timestamp2, url=url)
+    changed_routes_dict = changed_routes(hostname, service, timestamp1, timestamp2, url)
+    if added_deleted_routes_dict is not None:
+        logger.debug(f"Added and deleted routes: {added_deleted_routes_dict['added'][0]}")
+    else:
+        logger.debug(f"No added and deleted routes found")
+    
+    if changed_routes_dict is not None: 
+        logger.debug(f"Changed routes: {changed_routes_dict['changed'][0]}")
+    else:
+        logger.debug(f"No changed routes found")
     compared_routes = dict()
     compared_routes["added"] = added_deleted_routes_dict["added"]
     compared_routes["deleted"] = added_deleted_routes_dict["deleted"]
@@ -154,16 +172,22 @@ def get_added_deleted_routes(
     service: str,
     timestamp1: datetime,
     timestamp2: datetime,
-    fields: list = ["route"],
-    database_connection: sqlite3.connect = conn,
+    fields: list = ["hostname", "service", "route"],
+    url: str = default_database_url,
 ) -> dict:
-    routes1 = get_routes(hostname, service, timestamp1, database_connection)
-    routes2 = get_routes(hostname, service, timestamp2, database_connection)
+    # breakpoint()
+    logger.debug(f"Getting added and deleted routes for {hostname} {service} {timestamp1} {timestamp2}")
+    routes1 = get_routes(hostname, service, timestamp1, url)
+    if routes1 is not None:
+        logger.debug(f"Got routes for {hostname} {service} {timestamp1} first route: {routes1[0]}")
+    routes2 = get_routes(hostname, service, timestamp2, url)
+    if routes2 is not None:
+        logger.debug(f"Got routes for {hostname} {service} {timestamp1} first route: {routes2[0]}")
 
     if routes1 is None or routes2 is None:
         return None  # Or raise an exception if timestamps are invalid
-
     # Create a dictionary of unique identifiers for routes in timestamp1
+    
     unique_identifiers1 = {
         get_unique_identifier(route, fields): route for route in routes1
     }
@@ -187,7 +211,7 @@ def get_added_deleted_routes(
 
     return {
         "added": added_routes,
-        "removed": removed_routes,
+        "deleted": removed_routes,
     }
 
 
@@ -197,46 +221,47 @@ def changed_routes(
     timestamp1: datetime,
     timestamp2: datetime,
     fields: list = ["route"],
-    database_connection: sqlite3.connect = conn,
+    url: str = default_database_url,
 ) -> dict:
-    routes1 = get_routes(hostname, service, timestamp1, database_connection)
-    routes2 = get_routes(hostname, service, timestamp2, database_connection)
+    logger.debug(f"Getting changed routes for {hostname} {service} {timestamp1} {timestamp2}")
+    routes1 = get_routes(hostname, service, timestamp1, url)
+    routes2 = get_routes(hostname, service, timestamp2, url)
 
     if routes1 is None or routes2 is None:
         return None  # Or raise an exception if timestamps are invalid
+    with get_connection(url) as database_connection:
+        cursor = database_connection.cursor()
+        cursor.execute(
+            """
+            SELECT 
+                r1.route, 
+                r1.next_hop AS next_hop_before, r2.next_hop AS next_hop_after,
+                r1.metric AS metric_before, r2.metric AS metric_after,
+                r1.route_protocol AS route_protocol_before, r2.route_protocol AS route_protocol_after
+            FROM 
+                igp_routes r1
+            INNER JOIN 
+                igp_routes r2 
+            ON 
+                r1.route = r2.route 
+            WHERE 
+                r1.hostname = ? AND r1.service = ? AND r1.timestamp = ? AND 
+                r2.hostname = ? AND r2.service = ? AND r2.timestamp = ? AND 
+                (r1.next_hop != r2.next_hop OR 
+                r1.metric != r2.metric OR 
+                r1.route_protocol != r2.route_protocol)
+        """,
+            (hostname, service, timestamp1, hostname, service, timestamp2),
+        )
 
-    cursor = database_connection.cursor()
-    cursor.execute(
-        """
-        SELECT 
-            r1.route, 
-            r1.next_hop AS next_hop_before, r2.next_hop AS next_hop_after,
-            r1.metric AS metric_before, r2.metric AS metric_after,
-            r1.route_protocol AS route_protocol_before, r2.route_protocol AS route_protocol_after
-        FROM 
-            igp_routes r1
-        INNER JOIN 
-            igp_routes r2 
-        ON 
-            r1.route = r2.route 
-        WHERE 
-            r1.hostname = ? AND r1.service = ? AND r1.timestamp = ? AND 
-            r2.hostname = ? AND r2.service = ? AND r2.timestamp = ? AND 
-            (r1.next_hop != r2.next_hop OR 
-            r1.metric != r2.metric OR 
-            r1.route_protocol != r2.route_protocol)
-    """,
-        (hostname, service, timestamp1, hostname, service, timestamp2),
-    )
-
-    changed_routes = [
-        dict(zip([column[0] for column in cursor.description], row))
-        for row in cursor.fetchall()
-    ]
+        changed_routes = [
+            dict(zip([column[0] for column in cursor.description], row))
+            for row in cursor.fetchall()
+        ]
 
     return {"changed": changed_routes}
 
-def get_list_of_timestamps(hostname:str=None, database_connection: sqlite3.connect = conn) -> list:
+def get_list_of_timestamps(hostname:str=None, url: str = default_database_url) -> list:
     """
     Retrieves a list of timestamps for a given hostname from the database.
     If hostname is None, generate the list for all hostnames in the database. Order the results from newest to oldest.
@@ -247,18 +272,19 @@ def get_list_of_timestamps(hostname:str=None, database_connection: sqlite3.conne
         logger.debug(f"Getting list of timestamps for {hostname}") 
     else:
         logger.debug("Getting list of timestamps for all hostnames") 
-    cursor = database_connection.cursor()
-    if hostname is None:
-        cursor.execute("SELECT DISTINCT hostname, service, timestamp FROM igp_routes ORDER BY timestamp DESC")
-    else:
-        cursor.execute("SELECT DISTINCT hostname, service, timestamp FROM igp_routes WHERE hostname=? ORDER BY timestamp DESC", (hostname,))
+    with get_connection(url) as database_connection:
+        cursor = database_connection.cursor()
+        if hostname is None:
+            cursor.execute("SELECT DISTINCT hostname, service, timestamp FROM igp_routes ORDER BY timestamp DESC")
+        else:
+            cursor.execute("SELECT DISTINCT hostname, service, timestamp FROM igp_routes WHERE hostname=? ORDER BY timestamp DESC", (hostname,))
 
-    results = cursor.fetchall()
+        results = cursor.fetchall()
 
     return results
 
 
-def get_latest_timestamps(hostname: str, service:str, database_connection: sqlite3.connect = conn) -> tuple:
+def get_latest_timestamps(hostname: str, service:str, url: str = default_database_url) -> tuple:
     """
     Retrieves the two most recent timestamps for a given hostname and service combination from the database.
 
@@ -272,18 +298,19 @@ def get_latest_timestamps(hostname: str, service:str, database_connection: sqlit
     """
     
     logging.debug(f"Getting latest timestamps for {hostname} and {service}")
-    cursor = database_connection.cursor()
-    cursor.execute(
-        """
-        SELECT DISTINCT timestamp 
-        FROM igp_routes
-        WHERE hostname = ? AND service = ?
-        ORDER BY timestamp DESC
-        LIMIT 2
-        """,
-        (hostname, service),
-    )
-    results = cursor.fetchall()
+    with get_connection(url) as database_connection:
+        cursor = database_connection.cursor()
+        cursor.execute(
+            """
+            SELECT DISTINCT timestamp 
+            FROM igp_routes
+            WHERE hostname = ? AND service = ?
+            ORDER BY timestamp DESC
+            LIMIT 2
+            """,
+            (hostname, service),
+        )
+        results = cursor.fetchall()
 
     if len(results) == 2:
         return results[0][0], results[1][0]  # Extract timestamps from results
@@ -291,7 +318,7 @@ def get_latest_timestamps(hostname: str, service:str, database_connection: sqlit
         return None  # Or you might want to raise an exception
 
 
-def get_unique_identifier(route: dict, fields: list = None) -> str:
+def get_unique_identifier(route_dict: dict, fields: list = None) -> str:
     """Generates a unique identifier for a route based on the specified fields.
 
     Args:
@@ -301,12 +328,12 @@ def get_unique_identifier(route: dict, fields: list = None) -> str:
     Returns:
         A unique identifier string.
     """
-
     if fields is None:
-        fields = route.keys()
+        fields = route_dict.keys()
 
     # Sort the fields to ensure consistent ordering
     fields = sorted(fields)
-
     # Generate the identifier string
-    return f"{'-'.join([str(route[field]) for field in fields])}"
+    route_keys = [str(route_dict[field]) for field in fields]
+    identifier = f"{'-'.join(route_keys)}"
+    return identifier
