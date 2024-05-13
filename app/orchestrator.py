0480781c045873264d37abee400131b7c9e7f954
Viesur0 <victoresuarezr@gmail.com>
2024-05-12 23:57:34 -0600

    orchestrator.py Created a task to connect to devices and gather/scrap the output of the commands
    remote_command_execution function is the interface to generate the inventory of devices from a yaml file, commands from a yaml file and either executing a dry-run or actually connecting to the devices thorugh network_interface.py module

--- app/orchestrator.py
+++ app/orchestrator.py
@@ -1,26 +1,36 @@
+import os
 import logging
+
 logger = logging.getLogger(__name__)
+
+import yaml
 
 import storage
 database_url = "routes.sqlite3"
 storage.initialize_database(database_url)
 import netparser
 import file_operations
-
+import yaml_operations
+import network_interface
 
 def list_timestamps(hostname: str = None):
-    logging.info("list_timestamps")
+    logger.info("list_timestamps")
     if hostname:
         return storage.get_list_of_timestamps(hostname, url=database_url)
     return storage.get_list_of_timestamps(url=database_url)
+
 
 def fetch_single_device(ip_address: str):
     import network_interface
 
     pass
 
+
 def fetch_devices_from_file(filename: str):
+    import network_interface
+
     pass
+
 
 def load_routes_from_file(filename: str, hostname: str, timestamp: str, vendor: str):
     """
@@ -29,13 +39,12 @@ def load_routes_from_file(filename: str, hostname: str, timestamp: str, vendor: 
     :param timestamp: The timestamp to save to the database.
     :return: None
     """
-    logging.debug("load_routes_from_file")
-    content = file_operations.load_file(filename)
+    logger.debug("load_routes_from_file")
+    content = file_operations.load_file_content(filename)
     routes = netparser.parse(vendor, content, hostname, timestamp)
     logger.info(f"Loaded {len(routes)} routes from {filename}")
     storage.save_routes(timestamp, routes, url=database_url)
     logger.info(f"Saved {len(routes)} routes to the database")
-
 
 
 def compare_routes(hostname: str, service: str, timestamp1: str, timestamp2: str):
@@ -47,6 +56,45 @@ def compare_routes(hostname: str, service: str, timestamp1: str, timestamp2: str
     :param timestamp2: The second timestamp.
     :return: A dictionary containing the added, deleted, and changed routes.
     """
-    logging.info("compare_routes")
-    return storage.compare_routes(hostname, service, timestamp1, timestamp2, url=database_url)
+    logger.info("compare_routes")
+    return storage.compare_routes(
+        hostname, service, timestamp1, timestamp2, url=database_url
+    )
 
+
+def remote_command_execution(inventory_filename: str, command_filename: str, dry_run_flag: bool = False,):
+    """
+    Gather inventory of devices from a file.
+    :param filename: The file to load from.
+    :return: None
+    """
+    logger.debug("remote_command_execution")
+    inventory = yaml_operations.load_inventory(inventory_filename)
+    if not inventory:
+        logger.error("No inventory found")
+        return
+    commands = yaml_operations.load_commands(command_filename)
+    if not commands:
+        logger.error("No commands found")
+        return
+    
+    device_list = [x for x in yaml_operations.generate_device_list(inventory, commands)]
+    logger.info(f"Preparing to execute commands on {len(device_list)} devices")
+    logger.info(f"Devices to execute commands on: {[x.get('hostname') for x in device_list]}")
+    if dry_run_flag:
+        from pprint import pprint
+        logger.info("Dry run mode enabled")
+        logger.info(f"Inventory created: ##########")
+        pprint(inventory)
+        logger.info(f"Commands created: ##########")
+        pprint(commands)
+        logger.info(f"device_list created: ##########")
+        pprint(device_list)
+        logger.info("Dry run mode finished")
+        return
+
+    output_list = network_interface.execute_devices_commands(device_list)
+    logger.info(f"Commands output generated on {len(device_list)} devices")
+    logger.info(f"Ouput generated: {output_list}")
+
+    return output_list
