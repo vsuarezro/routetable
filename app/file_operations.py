e5fa5428267f9c482235d8a7ff174eb894ab6b9e
Viesur0 <victoresuarezr@gmail.com>
2024-05-12 19:49:36 -0600

    file_operations.py Renamed load_file for load_file_content
    load_file_content checks the file existance, file size and returns the file content of the file

--- app/file_operations.py
+++ app/file_operations.py
@@ -4,19 +4,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-ALLOWED_CHARACTERS = re.compile(r"^[a-zA-Z0-9\s\.,:-_\(\)\[\]#=\r\n]+$")  # Adjust as needed
 MAX_FILE_SIZE = 1024 * 1024 * 200  # 200 MB limit
 
-def load_file(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        if os.path.getsize(filename) > MAX_FILE_SIZE:
-            raise ValueError("File exceeds maximum size")
+def load_file_content(filename: str):
+    logger.debug("load_file_content")
 
-        contents = f.read()
-        # match = ALLOWED_CHARACTERS.match(contents, re.IGNORECASE | re.MULTILINE)
-        # logger.debug(f"File contains invalid characters: {match}")
-        # if not match:
-        #     # ALLOWED_CHARACTERS.match(contents, re.IGNORECASE | re.MULTILINE)
-        #     raise ValueError(f"File contains invalid characters {match}")
+    if not os.path.exists(filename):
+        logger.error(f"File {filename} does not exist")
+        raise FileNotFoundError("File {filename} does not exist")
 
-        return contents  # Proceed to process the sanitized content
+    if os.path.getsize(filename) > MAX_FILE_SIZE:
+        logger.error(f"File {filename} exceeds maximum size")
+        raise ValueError(f"File {filename} exceeds maximum size")
+        return
+
+    with open(filename, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    logger.debug(f"File {filename} loaded successfully")
+    return content
