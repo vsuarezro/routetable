26b4830941eeaf675bb5d30cfda23d1df86a9dba
Viesur0 <victoresuarezr@gmail.com>
2024-05-13 13:12:14 -0600

    black cli.py. No meaningful change

--- app/cli.py
+++ app/cli.py
@@ -25,13 +25,29 @@ def validate_timestamp(timestamp):
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Networks tools. Compare and Screape"
-    )
+    parser = argparse.ArgumentParser(description="Networks tools. Compare and Screape")
     # main options, valid for more than one command
-    parser.add_argument("-i","--inventory-file", type=str, default="inventory.yaml", help="YAML file with device information")
-    parser.add_argument("-c", "--commands-file", type=str, default="command.yaml", help="YAML file with commands to execute")
-    parser.add_argument("-f","--device-filter", type=str, default="all", help="Filter devices (hostname or role)")
+    parser.add_argument(
+        "-i",
+        "--inventory-file",
+        type=str,
+        default="inventory.yaml",
+        help="YAML file with device information",
+    )
+    parser.add_argument(
+        "-c",
+        "--commands-file",
+        type=str,
+        default="command.yaml",
+        help="YAML file with commands to execute",
+    )
+    parser.add_argument(
+        "-f",
+        "--device-filter",
+        type=str,
+        default="all",
+        help="Filter devices (hostname or role)",
+    )
 
     # Main commands: compare and scrape
     subparsers = parser.add_subparsers(title="Commands", dest="command")
@@ -40,21 +56,46 @@ def main():
     parser_scrape = subparsers.add_parser("scrape", help="Scrape device configurations")
 
     # Scrape mutually exclusive options
-    scrape_output_group = parser_scrape.add_mutually_exclusive_group()  # Mutually exclusive group within 'scrape'
-    scrape_output_group.add_argument("--scrape-output", choices=["per-device", "per-command", "single-file"], default="per-device", help="Select the number of files to create where to save the outputs, per-device saves a single file per device, per-command saves a file per combination of hostname+command and single-file saves a single file with all the outputs. Default is per-device")
-    scrape_output_group.add_argument("--dry-run", action="store_true", default=False, help="Simulate scrape without connecting")
+    scrape_output_group = (
+        parser_scrape.add_mutually_exclusive_group()
+    )  # Mutually exclusive group within 'scrape'
+    scrape_output_group.add_argument(
+        "--scrape-output",
+        choices=["per-device", "per-command", "single-file"],
+        default="per-device",
+        help="Select the number of files to create where to save the outputs, per-device saves a single file per device, per-command saves a file per combination of hostname+command and single-file saves a single file with all the outputs. Default is per-device",
+    )
+    scrape_output_group.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="Simulate scrape without connecting",
+    )
 
     # Compare Command
     parser_compare = subparsers.add_parser("compare", help="Compare configurations")
-    parser_compare.add_argument("--table", choices=["route","bgp",], default="route", help="Select route-table or bgp-table to compare. Defaults to route"
-                                )
-    parser_compare.add_argument("--compare-output", choices=["text", "csv", "yaml", "json", "xml", "table"], default="text", help="Select output format (for compare command)",
-                                )
-    compare_group = parser_compare.add_mutually_exclusive_group()  # Mutually exclusive group within 'compare'
+    parser_compare.add_argument(
+        "--table",
+        choices=[
+            "route",
+            "bgp",
+        ],
+        default="route",
+        help="Select route-table or bgp-table to compare. Defaults to route",
+    )
+    parser_compare.add_argument(
+        "--compare-output",
+        choices=["text", "csv", "yaml", "json", "xml", "table"],
+        default="text",
+        help="Select output format (for compare command)",
+    )
+    compare_group = (
+        parser_compare.add_mutually_exclusive_group()
+    )  # Mutually exclusive group within 'compare'
     compare_group.add_argument(
         "--query",
         nargs=4,
-        metavar=("HOSTNAME","SERVICE","TIMESTAMP1", "TIMESTAMP2"),
+        metavar=("HOSTNAME", "SERVICE", "TIMESTAMP1", "TIMESTAMP2"),
         help="Compare routes between two timestamps",
     )
     compare_group.add_argument(
@@ -79,10 +120,9 @@ def main():
     compare_group.add_argument(
         "--remove",
         nargs=2,
-        metavar=("HOSTNAME","TIMESTAMP"),
+        metavar=("HOSTNAME", "TIMESTAMP"),
         help="Remove specific compare checkpoints from the database",
     )
-
 
     # Logging options
     logging_group = parser.add_mutually_exclusive_group()
@@ -95,7 +135,7 @@ def main():
         action="store_true",
         help="Suppress output except critical errors",
     )
-    
+
     args = parser.parse_args()
     print(args)
 
@@ -107,22 +147,30 @@ def main():
         logging.basicConfig(level=logging.ERROR)
         logger.setLevel(logging.ERROR)
     else:
-        logging.basicConfig(level=logging.INFO, 
-                            format="%(name)s - %(levelname)s - %(message)s",
-                            )
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(name)s - %(levelname)s - %(message)s",
+        )
         logger.setLevel(logging.INFO)
 
     if args.command == "scrape":
         logger.info("Starting CLI scrape command")
-        output_list = orchestrator.remote_command_execution(args.inventory_file, args.commands_file, args.device_filter, args.dry_run,)
+        output_list = orchestrator.remote_command_execution(
+            args.inventory_file,
+            args.commands_file,
+            args.device_filter,
+            args.dry_run,
+        )
         if args.dry_run:
             exit()
         if output_list is None or len(output_list) == 0 or output_list is [None]:
             logger.error("No output received")
             exit()
-        devices_executed = [x.get('hostname') for x in output_list]
+        devices_executed = [x.get("hostname") for x in output_list]
         logger.info(f"Commands executed on {len(devices_executed)} devices")
-        logger.info(f"Commands executed on devices: {' '.join(devices_executed)} devices")
+        logger.info(
+            f"Commands executed on devices: {' '.join(devices_executed)} devices"
+        )
 
         output_formatter = formatter.scrape_formatter_function.get(args.scrape_output)
         tuples_list = output_formatter(output_list)
@@ -131,23 +179,30 @@ def main():
                 f.write(output)
                 print(f"File {filename}\nOUTPUT: {output}")
 
-
     if args.command == "compare":
         logger.info("Starting compare command")
         logger.info(f"Reference table:{args.table}")
-        if not args.list and not args.query and not args.load_file and not args.fetch and not args.remove:
+        if (
+            not args.list
+            and not args.query
+            and not args.load_file
+            and not args.fetch
+            and not args.remove
+        ):
             args.list = "all"
 
         if args.list:
             logger.info("Listing available timestamps")
-            timestamp_list = orchestrator.list_timestamps(None if args.list == "all" else args.list)
+            timestamp_list = orchestrator.list_timestamps(
+                None if args.list == "all" else args.list
+            )
             if timestamp_list is None or len(timestamp_list) == 0:
                 logger.warning("No timestamps found")
                 return
             for timestamp in timestamp_list:
                 print(" ".join([str(x) for x in timestamp]))
             exit()
-    
+
         if args.load_file:
             logger.info("Loading routes from file")
 
@@ -203,12 +258,9 @@ def main():
                     f"{timestamp} is not a valid timestamp. format is YYYY-MM-DD_HH:MM"
                 )
                 exit()
-            entries_deleted = orchestrator.remove_routes_for_device(
-                hostname, timestamp
-            )
+            entries_deleted = orchestrator.remove_routes_for_device(hostname, timestamp)
             logger.info(f"Entries deleted: {entries_deleted}")
             exit()
-
 
     exit()
 
